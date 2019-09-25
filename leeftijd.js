432ce9d9b7af96e39f8383ba902e698bded67c4a
Wesley <54977819+99030063@users.noreply.github.com>
2019-09-25 09:28:20 +0200

    initial upload

---
+++ leeftijd.js
@@ -0,0 +1,17 @@
+var leeftijd=prompt('hoe oud ben je?')
+if (leeftijd<21){
+    if(leeftijd<18){ 
+        if(leeftijd<16){
+            alert('nope')
+        }else{
+            alert('eerste verdieping')
+        }
+
+    }else{
+        alert('tweede verdieping')
+
+    }
+
+}else{
+        alert('VIP verdieping')
+}
